fe123fa503f0ef426a2d79aa042a3e282a8ed40e
VergaraC <victorvaac@al.insper.edu.br>
2021-10-31 21:40:00 -0300

    init

---
+++ main.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+class Disciplina(BaseModel):
+    """
+    This represents the object stored in the database.
+    """
+    nome: str
+    nome_professor: Optional[str] = None
+    anotacoes: str
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
