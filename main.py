8c9cb91aae0f5aaccfe2a806c15f719c6b2c67c6
VergaraC <victorvaac@al.insper.edu.br>
2021-11-08 22:42:08 -0300

    Comentario n vai nos structs

--- main.py
+++ main.py
@@ -1,26 +1,119 @@
+from fastapi import FastAPI,HTTPException, Path
 from typing import Optional
+from pydantic import BaseModel
+class Disciplina(BaseModel):
+    nome: str
+    professor: Optional[str] = None
+    comentario: str
 
-from fastapi import FastAPI
+class Nota(BaseModel):
+    materia: str
+    nota: float
+
+ex_d = Disciplina(nome="Megadados",professor="Ayres",comentario="Entrega para 8/11")
+ex_n = Nota(materia="Megadados",nota=10.0)
+
+disciplinas = [ex_d]
+notas = [ex_n]
+
 
 app = FastAPI()
 
-class Disciplina(BaseModel):
-    """
-    This represents the object stored in the database.
-    """
-    nome: str
-    nome_professor: Optional[str] = None
-    anotacoes: str
-
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def Home():
+    return {"Mensagem":"Bem vindo!"}
 
+@app.get("/disciplinas")
+async def getDisciplinas():
+    return disciplinas
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/disciplinas/{nome}")
+async def getDisciplinaByName(nome: str = Path(None, title="Nome", description="Nome da disciplina")):
+    for disciplina in disciplinas:
+        if disciplina.nome == nome:
+            return disciplina
+    raise HTTPException(status_code = 404, detail = "'nome' not found")
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+@app.post("/disciplinas")
+async def createDisciplinas(item: Disciplina ):
+    for disciplina in  disciplinas:
+        if disciplina.nome == item.nome:
+            raise HTTPException(status_code = 400, detail = "'nome' already in use")
+    disciplinas.append(item)
+    return item
+
+@app.delete("/disciplinas/{nome}")
+async def delDisciplina(nome: str = Path(None, title="Nome", description="Nome da disciplina")):
+    for disciplina in disciplinas:
+        if disciplina.nome == nome:
+            disciplinas.pop(disciplinas.index(disciplina))
+            return {"Success":"Disciplina deleted"}
+    raise HTTPException(status_code = 404, detail = "'nome' not found")
+
+@app.get("/disciplinas/get-nomes/")
+async def getListaDisciplinas():
+    list_disciplinas_nomes = []
+    for disciplina in disciplinas:
+        list_disciplinas_nomes.append(disciplina.nome)
+    return list_disciplinas_nomes
+
+@app.put("/disciplinas/{nome}")
+async def updateDisciplina(nome: str , item: Disciplina):
+    idx = 0
+    for disciplina in disciplinas:
+        if disciplina.nome == nome:
+            disciplinas[idx] = item
+            return  disciplinas[idx]
+        idx += 1
+    raise HTTPException(status_code = 404, detail = "'nome' not found")
+
+#########################################################################
+############################   Funções  ################################# 
+
+def getLastId(notas):
+    if notas:
+        ultima_nota = notas[-1]
+        return ultima_nota["id"]
+    return 0
+
+#########################################################################
+#########################################################################
+
+@app.post("/notas")
+async def createNotas(item: Nota):   
+    for d in disciplinas:
+        if d.nome == item.materia:
+            new = item.dict()
+            ultimo_id = getLastId(notas)
+            new["id"] = ultimo_id+1
+            notas.append(new)
+            return new
+    raise HTTPException(status_code = 404, detail = "'disciplina' not found")
+
+@app.delete("/notas/{id}")
+async def delNota(id: int = Path(None, title="Id", description="Id Nota")):
+    for nota in notas:
+        if nota["id"] == id:
+            notas.pop(notas.index(nota))
+            return {"message":"nota has been deleted"}
+    raise HTTPException(status_code = 404, detail = "'nota id' not found")
+
+@app.get("/notas")
+async def getNotas():
+    return notas
+
+@app.put("/notas/{id}")
+async def updateNota(item: Nota, id: int):
+    idx = 0
+    for nota in notas:
+        if nota["id"] == id:
+            for d in disciplinas:
+                if d.nome == item.materia:
+                    update = item.dict()
+                    update["id"] = id
+                    notas[idx] = update
+                    return notas[idx]
+            raise HTTPException(status_code = 404, detail="'disciplina' not found")
+        idx += 1
+    raise HTTPException(status_code = 404, detail = "'nota id' not found")
+
