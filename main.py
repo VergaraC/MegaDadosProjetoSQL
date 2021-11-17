36817d87b78e1769c1d337032338ca289f4cafff
VergaraC <victorvaac@al.insper.edu.br>
2021-11-17 17:22:34 -0300

    K

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from fastapi import FastAPI,HTTPException, Path
 from typing import Optional
 from pydantic import BaseModel
+
+from os import name
+from fastapi import FastAPI,status,HTTPException, APIRouter,Depends
+
+from typing import Optional, List
+from pydantic import BaseModel
+import uvicorn
+
+from src.model.subject import Subject
+from src.crud.utils import ExistenceException, NonExistenceException
+from src.schema.subject import SubjectInDB, SubjectOutDB,SubjectCreate,SubjectUpdate
+from src.schema.note import NoteInDB, NoteOutDB,NoteCreate,NoteUpdate
+from src.database.database import Base, engine,SessionLocal
+from src.crud.subject import get_all_subjects,get_subject,create_subject
+from sqlalchemy.orm import Session
+
+
 class Disciplina(BaseModel):
     nome: str
     professor: Optional[str] = None
@@ -17,24 +34,24 @@ disciplinas = [ex_d]
 notas = [ex_n]
 
 
-app = FastAPI()
+appDisciplinas = FastAPI()
 
-@app.get("/")
+@appDisciplinas.get("/")
 async def Home():
     return {"Mensagem":"Bem vindo!"}
 
-@app.get("/disciplinas")
+@appDisciplinas.get("/disciplinas")
 async def getDisciplinas():
     return disciplinas
 
-@app.get("/disciplinas/{nome}")
+@appDisciplinas.get("/disciplinas/{nome}")
 async def getDisciplinaByName(nome: str = Path(None, title="Nome", description="Nome da disciplina")):
     for disciplina in disciplinas:
         if disciplina.nome == nome:
             return disciplina
     raise HTTPException(status_code = 404, detail = "'nome' not found")
 
-@app.post("/disciplinas")
+@appDisciplinas.post("/disciplinas")
 async def createDisciplinas(item: Disciplina ):
     for disciplina in  disciplinas:
         if disciplina.nome == item.nome:
@@ -42,7 +59,7 @@ async def createDisciplinas(item: Disciplina ):
     disciplinas.append(item)
     return item
 
-@app.delete("/disciplinas/{nome}")
+@appDisciplinas.delete("/disciplinas/{nome}")
 async def delDisciplina(nome: str = Path(None, title="Nome", description="Nome da disciplina")):
     for disciplina in disciplinas:
         if disciplina.nome == nome:
@@ -50,14 +67,14 @@ async def delDisciplina(nome: str = Path(None, title="Nome", description="Nome d
             return {"Success":"Disciplina deleted"}
     raise HTTPException(status_code = 404, detail = "'nome' not found")
 
-@app.get("/disciplinas/get-nomes/")
+@appDisciplinas.get("/disciplinas/get-nomes/")
 async def getListaDisciplinas():
     list_disciplinas_nomes = []
     for disciplina in disciplinas:
         list_disciplinas_nomes.append(disciplina.nome)
     return list_disciplinas_nomes
 
-@app.put("/disciplinas/{nome}")
+@appDisciplinas.put("/disciplinas/{nome}")
 async def updateDisciplina(nome: str , item: Disciplina):
     idx = 0
     for disciplina in disciplinas:
@@ -78,6 +95,9 @@ def getLastId(notas):
 
 #########################################################################
 #########################################################################
+
+appNotas = FastAPI()
+
 
 @app.post("/notas")
 async def createNotas(item: Nota):   
@@ -117,3 +137,9 @@ async def updateNota(item: Nota, id: int):
         idx += 1
     raise HTTPException(status_code = 404, detail = "'nota id' not found")
 
+
+app.include_router(router_Note, prefix="/Note",tags=["Notes"])
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=5000)
